c4f197341715c4375b2669b5b10a8f9d9e8fa1c0
bdg <bdg>
2023-08-28 16:05:05 +0800

    init

--- dlkoo.js
+++ dlkoo.js
@@ -1,6 +1,8 @@
+let fs = require('fs')
+let path = require('path')
 let axios = require('axios')
 let cheerio = require('cheerio')
-const fs = require('fs')
+let puppeteer = require('puppeteer')
 
 let cookie = ''
 
@@ -10,41 +12,62 @@ function wait (ms = 2100) {
   )
 }
 
+// 随便打开一个网址 目的是获取cookie
+// return true 获取成功 默认修改cookie变量
+async function doGetCookie () {
+  let _getCookie = async () => {
+
+    const browser = await puppeteer.launch({ headless: false })
+    const page = await browser.newPage()
+    page.setViewport({
+      width: 1280,
+      height: 1024,
+      deviceScaleFactor: 1,
+    })
+
+    await page.setExtraHTTPHeaders({
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36',
+      'Referer': 'https://www.dlkoo.cc/down/2/2023/619911099.html',
+    })
+
+    await page.goto('https://www.dlkoo.cc/down/downfile.asp?did=546832')
+
+    while (true) {
+      // 等待用户输入验证码 页面会自动跳转
+      await page.waitForNavigation({ timeout: 0 })
+
+      // 页面带有.blue 表示提供了下载链接 应该是验证成功了
+      const isSuccess = await page.evaluate(() => {
+        return document.getElementsByClassName('blue').length != 0
+      })
+
+      if (isSuccess) {
+        let c = await page.cookies()
+        c = c[0]
+        cookie = `${c.name}=${c.value}`
+        break
+      }
+
+      await page.goBack()
+      await page.reload()
+    }
+
+    await browser.close()
+    return true
+  }
+
+  while (true) {
+    try {
+      let r = await _getCookie()
+      if (r)
+        return
+    } catch (e) {
+      console.log(e)
+    }
+  }
+}
+
 setTimeout(async () => {
-  // const gm = require('gm')
-
-  // async function denoise (image, threshold = 40) {
-  //   return new Promise((resolve, reject) => {
-  //     gm(image)
-  //       .threshold(threshold, '%')
-  //       .write(image, function (err) {
-  //         if (!err) {
-  //           resolve()
-  //         } else {
-  //           reject(err)
-  //         }
-  //       })
-  //   })
-  // }
-
-  // await denoise('0.bmp')
-
-
-  // let Tesseract = require('tesseract.js')
-
-  // let r = await Tesseract.recognize(
-  //   'https://tesseract.projectnaptha.com/img/eng_bw.png',
-  //   'eng',
-  //   { logger: m => console.log(m) }
-  // )
-  // console.log(r)
-
-  // await wait(10000)
-
-  // return
-
-
-
   let filePath = ''
   let fileName = ''
   let headers = {
@@ -57,6 +80,8 @@ setTimeout(async () => {
     //"Referer": "",
   }
 
+  // return true 成功下载到torrent
+  // return false cookie失效 上层获取cookie
   let doDownload = async (href, refer) => {
 
     await wait()
@@ -68,10 +93,6 @@ setTimeout(async () => {
     let $ = cheerio.load(c.data)
     fs.writeFileSync('dl.log', c.data)
     let movid = $('input[id="movid"]').attr('value')
-
-    // 验证 获取cookie
-    // 假设用户已经有了cookie
-    let codestr = null
 
     await wait()
     headers.Referer = `${url}`
@@ -85,9 +106,9 @@ setTimeout(async () => {
       ac: 'b',
       movid: +movid,
     }
-    if (codestr)
-      postBody['codestr'] = codestr
     c = await axios.post(url, postBody, { headers })
+    if (c.data === '')
+      return false
     $ = cheerio.load(c.data)
     let downloads = $('a.blue')
     for (let download of downloads) {
@@ -95,7 +116,7 @@ setTimeout(async () => {
       let href = $(download).attr('href')
       let c = await axios.get(`https://www.dlkoo.cc/down/${href}`, { headers, responseType: 'arraybuffer' })
 
-      let pth = `dlkoo//${filePath}//`
+      let pth = path.join('dlkoo', filePath.replace(/\//g, ''))
       try {
         if (!fs.existsSync(pth)) {
           fs.mkdirSync(pth, { recursive: true })
@@ -104,13 +125,14 @@ setTimeout(async () => {
         console.log(`path ${pth} create failed!!!`)
         continue
       }
-      fs.writeFileSync(`${pth}//${fileName}.torrent`, c.data, 'binary')
+      fs.writeFileSync(path.join(pth, `${fileName}.torrent`), c.data, 'binary')
     }
 
     delete headers.Referer
     delete headers.Origin
     delete headers.Cookie
     delete headers['Content-Type']
+    return true
   }
 
   // 每个电影的详细页面
@@ -121,14 +143,16 @@ setTimeout(async () => {
     console.log(url)
     let c = await axios.get(url)
     let $ = cheerio.load(c.data)
-    // let downloads = $('a[class="downtitle"]').attr('href')
     let downloads = $('a[class="downtitle"]')
     for (let download of downloads) {
       let href = $(download).attr('href')
       fileName = $(download).text()
-      await doDownload(href, url)
+      let r = await doDownload(href, url)
+      if (r === false) {
+        await doGetCookie()
+        await doDownload(href, url)
+      }
     }
-    // await doDownload(downloads, url)
   }
 
   for (let i = 0; i < 100; ++i) {
