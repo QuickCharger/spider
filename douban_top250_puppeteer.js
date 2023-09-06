33eaf992bf960c5e6641d14f5f65ee671a187159
bdg <bdg>
2023-09-06 17:44:26 +0800

    init

--- douban_top250_puppeteer.js
+++ douban_top250_puppeteer.js
@@ -1,13 +1,15 @@
 let axios = require('axios')
 let cheerio = require('cheerio')
 let puppeteer = require('puppeteer')
-let { sleep } = require('./easy')
+let easy = require('./easy.js')
+let Path = require('path')
+let { createPath, save, sleep, newPageNeedBind } = easy
+
+let needLogin = false
 
 setTimeout(async () => {
 
-  // 登陆！！！
-
-  const browser = await puppeteer.launch({
+  let _browser = await puppeteer.launch({
     headless: false,
     args: [
       // '--start-maximized',
@@ -15,44 +17,58 @@ setTimeout(async () => {
       '--window-size=1280,1024',
     ],
   })
+  let _browser_headless = await puppeteer.launch({ headless: 'new' })
 
-  let newPage = async (url) => {
-    let page = await browser.newPage()
-    page.setViewport({
-      width: 1280,
-      height: 1024,
-      deviceScaleFactor: 1,
-    })
-
-    await page.goto(url, { waitUntil: 'networkidle2' })
-    await page.addScriptTag({ url: 'https://cdn.bootcdn.net/ajax/libs/jquery/3.6.4/jquery.min.js' })
-    await page.waitForSelector('.nav-user-account')
-    return page
-  }
+  let newPage = newPageNeedBind.bind(null, _browser)
+  let newPageHeadless = newPageNeedBind.bind(null, _browser_headless)
 
   let start = '?start=0&filter='
   while (start.length > 0) {
-    let page = await newPage(`https://movie.douban.com/top250${start}`)
+    let cookie = easy.load('douban', 'cookie.txt', JSON.stringify([]))
+    let page = await newPage({ url: `https://movie.douban.com/top250${start}`, cookie: JSON.parse(cookie), media: true, waitUntil: 'domcontentloaded' })
+
+    // 等待登陆
+    if (needLogin) {
+      await page.evaluate(() => {
+        window.alert('先登陆')
+      })
+      await page.waitForSelector('.nav-user-account', { timeout: 0 })
+      let cookie = await page.cookies()
+      await easy.save('douban', 'cookie.txt', JSON.stringify(cookie))
+    }
 
     let lis = await page.evaluate(() => {
       let ret = []
-      let lis = $('.grid_view > li')
+      let lis = $('.grid_view > li').toArray()
       for (let li of lis) {
         let rank = $(li).find('.item > .pic > em').text()
         let name = $(li).find('.item > .info > .hd > a > .title').eq(0).text()
         let href = $(li).find('.item > .info > .hd > a').attr('href')
-        ret.push({ rank, name, href })
+        let id = 'id' + Math.random().toString(36).substr(2, 9) // 创建一个随机ID 不能数字开头 所以前面加'id'
+        li.setAttribute('id', id)
+        ret.push({ rank, name, href, id })
       }
       return ret
     })
 
     for (let li of lis) {
-      let page = await newPage(li.href)
-      let rank = await page.evaluate(() => {
+      let p = await newPageHeadless({ url: li.href, useWindow: true, media: true, waitUntil: 'domcontentloaded' })
+      // await page.$eval(`#${li.id}`, (li) => {
+      //   // console.log(li)
+      //   // return
+      //   window.customBlink(li)
+      // })
+      page.evaluate((id) => {
+        window.customBlink(id)
+      }, li.id)
+      let rank = await p.evaluate(() => {
         let rank = $('.ll.rating_num').text()
         return rank
       })
-      console.log(rank)
+      console.log(JSON.stringify(li))
+      await createPath('douban')
+      await p.screenshot({ path: Path.join('douban', `_${li.name}.png`), fullPage: true })
+      await p.close()
     }
 
     start = await page.evaluate(() => {
@@ -69,5 +85,5 @@ setTimeout(async () => {
     await sleep(500)
   }
 
-  await browser.close()
-}, 100)
+  await _browser.close()
+}, 1000)
--- easy.js
+++ easy.js
@@ -1,26 +1,35 @@
-const { writeFile, readFile } = require('fs/promises')
+let path = require('path')
+let fs = require('fs')
 
-const saveCookie = async (page, website) => {
-  await page.waitForFunction(() => {
-    if (window.x) return true
-  }, { timeout: 0 })
-
-  const cookie = await page.cookies()
-
-  // 这里统一把所有网站的cookie写入cookies文件夹里
-  await writeFile(`./cookies/${website}.json`, JSON.stringify(cookie))
+let createPath = async (pth) => {
+  try {
+    if (!fs.existsSync(pth)) {
+      fs.mkdirSync(pth, { recursive: true })
+    }
+  } catch (e) {
+    console.log(`path ${pth} create failed!!!`)
+    return false
+  }
+  return true
 }
 
-const getCookie = async (website) => {
-  const path = `./cookies/${website}.json`
-  const data = await readFile(`./cookies/${website}.json`, {
-    flag: 'a+'
-  })
+let save = async (pth, filename, content, utf8 = true) => {
+  let r = await createPath(pth)
+  if (!r)
+    return false
+  fs.writeFileSync(path.join(pth, filename), content, utf8 ? 'utf8' : 'binary')
+  return true
+}
 
-  // 这里可以再加入判断cookie是否过期,如果过期,则也直接return的逻辑
-  if (!data.toString()) return
-
-  return JSON.parse(data.toString())
+let load = (pth, filename, defaul, utf8 = true) => {
+  try {
+    let content = fs.readFileSync(path.join(pth, filename), utf8 ? 'utf8' : 'binary')
+    return content
+  } catch (e) {
+    console.log(e)
+    return defaul
+  }
+  return ''
 }
 
 function sleep (ms = 1000, cb = null) {
@@ -36,7 +45,89 @@ function sleep (ms = 1000, cb = null) {
 
 
 
+/**
+ * @param {*} waitUntil
+ *              load https://developer.mozilla.org/en-US/docs/Web/API/Window/load_event
+ *                全部网页load，包含cs，js，img。 不包含异步加载！！！
+ *              domcontentloaded  https://developer.mozilla.org/en-US/docs/Web/API/Document/DOMContentLoaded_event
+ *                HTML document全部解析，包含异步js加载。不等待img
+ *              networkidle0 consider setting content to be finished when there are no more than 0 network connections for at least `500` ms
+ *              networkidle2 consider setting content to be finished when there are no more than 2 network connections for at least `500` ms.
+ * @returns 
+ */
+let newPageNeedBind = async (browser, { url, cookie = [], media = true, waitUntil = 'networkidle2' }) => {
+  let page = await browser.newPage()
+  page.setViewport({
+    width: 1280,
+    height: 1024,
+    deviceScaleFactor: 1,
+  })
+
+  if (!media) {
+    await page.setRequestInterception(true)
+    page.on('request', (request) => {
+      if (request.resourceType() === 'image' || request.resourceType() === 'media') {
+        request.abort()
+      } else {
+        request.continue()
+      }
+    })
+  }
+
+  await page.setCookie(...cookie)
+  await page.goto(url, { waitUntil })
+  await page.addScriptTag({ url: 'https://cdn.bootcdn.net/ajax/libs/jquery/3.6.4/jquery.min.js' })
+
+  // to use
+  // await page.evaluate(()=>{ window.customBlink(...)})
+  await page.evaluate(() => {
+    window.customBlink = (id, fadeOutTime = 500, fadeInTime = 500, delay = 0, callback = null) => {
+      console.log(`blink id ${id}`)
+      let ele = $(`#${id}`) // 选择你的元素
+      // ele.animate({
+      //   backgroundColor: 'pink' // 改变底色为红色
+      // }, 500) // 这个动画持续0.5秒
+      //   .animate({
+      //     backgroundColor: '#ffffff' // 改变底色回到白色
+      //   }, 500)
+      // $(window).scrollTop(ele.offset().top - $(window).height() / 3)
+      $(window).scrollTop(ele.offset().top - $(window).height() / 2)
+      ele.css("background", "pink")
+
+      // setInterval(function () {
+      //   $element.animate({
+      //     backgroundColor: '#ff0000' // 改变到的底色
+      //   }, 500) // 半秒钟淡出
+      //     .animate({
+      //       backgroundColor: '#ffffff' // 原始的底色
+      //     }, 500) // 半秒钟淡入
+      // }, 1000) // 每秒钟重复一次
+    }
+  })
+  // await page.exposeFunction('customBlink', (id, fadeOutTime = 500, fadeInTime = 500, delay = 0, callback = null) => {
+  //   $(id).fadeOut(fadeOutTime, function () {
+  //     $(this).fadeIn(fadeInTime, function () {
+  //       setTimeout(() => {
+  //         $(this).fadeOut(fadeOutTime, function () {
+  //           $(this).fadeIn(fadeInTime, function () {
+  //             customBlink(this, fadeOutTime, fadeInTime, delay, callback)
+  //           })
+  //         })
+  //       }, delay)
+  //     })
+  //   })
+  //   if (callback) {
+  //     callback()
+  //   }
+  // })
+
+  return page
+}
+
+
 module.exports = {
-  saveCookie, getCookie,
-  sleep
+  createPath,
+  save, load,
+  sleep,
+  newPageNeedBind
 }
